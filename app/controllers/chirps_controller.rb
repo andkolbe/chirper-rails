101bf5ad421f38f430316e520faf355873748e44
Andrew <kolbe1129@gmail.com>
2021-10-04 17:23:15 -0500

    GET a single chirp

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -1,4 +1,12 @@
 class ChirpsController < ApplicationController
   def index
+    @chirps = Chirp.all
+  end
+
+  def show
+    @chirp = Chirp.find(params[:id])
   end
 end
+
+
+# index and show are called actions. These map to views with the same name
