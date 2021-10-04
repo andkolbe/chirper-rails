13ed3bb94d7dcde69801a330d7f1dc25c57ab16e
Andrew <kolbe1129@gmail.com>
2021-10-04 16:22:23 -0500

    created Chirps route, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "chirps#index"
+  get "/chirps", to: "chirps#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
