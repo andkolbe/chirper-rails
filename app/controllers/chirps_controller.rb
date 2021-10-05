c5cdd4c4c4488f19619ba7b4b869689667ae6888
Andrew <kolbe1129@gmail.com>
2021-10-04 23:10:05 -0500

    added edit and update actions

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -23,6 +23,24 @@ class ChirpsController < ApplicationController
     end
   end
 
+  # display the page to edit an existing chirp
+  # Fetches the chirp from the database and stores it in @chirp, so that it can be used when building the form
+  def edit 
+    @chirp = Chirp.find(params[:id])
+  end
+
+  # PUT request to update a chirp
+  # Refetch the article from the database and attempts to update it with the submitted form data filtered by chirp_params
+  def update
+    @chirp = Chirp.find(params[:id])
+
+    if @chirp.update(chirp_params)
+      redirect_to @chirp
+    else
+      render :edit
+    end
+  end
+
   private 
     def chirp_params
       params.require(:chirp).permit(:content, :location)
